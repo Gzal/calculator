f4c9af10094498ccc022fa06392931765ffc2788
Gzal <14190835+Gzal@users.noreply.github.com>
2017-12-13 07:52:20 -0800

    Add input handling file

---
+++ Calculator/Calculator/input.cpp
@@ -0,0 +1,9 @@
+//
+//  input.cpp
+//  Calculator
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 12/13/17.
+//  Copyright © 2017 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#include "inputs.hpp"
---
+++ Calculator/Calculator/input.hpp
@@ -0,0 +1,17 @@
+//
+//  input.hpp
+//  Calculator
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 12/13/17.
+//  Copyright © 2017 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#ifndef inputs_hpp
+#define inputs_hpp
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#endif /* inputs_hpp */
